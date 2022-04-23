6ec6db2aeebb8d15b324f9b4ce1ac2e1a007517a
hieutrn1205 <46225980+hieutrn1205@users.noreply.github.com>
2022-04-22 21:02:16 -0400

    Added predictions for column Tips

--- Coconana.py
+++ Coconana.py
@@ -46,6 +46,13 @@ sales
 # In[5]:
 
 
+#sales["Date"] = pd.to_datetime(sales["Date"], infer_datetime_format = True, errors = 'coerce')
+#sales.head()
+
+
+# In[6]:
+
+
 #Remove special characters to convert the value in the Net Total column 
 #into the numeric value in order to graph
 sales["Net Total"]= sales["Net Total"].str.replace("$", "")
@@ -58,7 +65,7 @@ plt.xlabel("# of Order")
 plt.ylabel("Net Total($)")
 
 
-# In[6]:
+# In[7]:
 
 
 sales["Net Total"].hist(bins = 15)
@@ -67,7 +74,7 @@ plt.xlabel("Net Total ($)")
 plt.ylabel("Frequency")
 
 
-# In[7]:
+# In[8]:
 
 
 sales["Tips"] = sales["Tips"].str.replace("$", "")
@@ -76,11 +83,228 @@ lm = smf.ols("Tips ~ Q('Order Count') + Q('Net Total')", data=sales).fit()
 lm.summary()
 
 
-# In[8]:
+# In[9]:
 
 
 plt.scatter(x = sales["Tips"], y = lm.resid)
 plt.title("Relationship between Net Total, Order Count and Tips")
 plt.xlabel("Tips")
 plt.ylabel("Residuals")
+
+
+# In[10]:
+
+
+sales.head()
+
+
+# In[11]:
+
+
+sales.dtypes
+
+
+# In[12]:
+
+
+sales["Gross Sales"] = sales["Gross Sales"].str.replace("$", "")
+sales["Gross Sales"] = sales["Gross Sales"].str.replace(",", "")
+sales["Gross Sales"] = sales["Gross Sales"].astype(float)
+sales.head()
+
+
+# In[13]:
+
+
+x = sales[["Order Count", "Gross Sales", "Net Total"]]
+y = sales["Tips"]
+
+
+# In[14]:
+
+
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error
+
+from sklearn.metrics import confusion_matrix
+
+
+# In[15]:
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+
+# In[16]:
+
+
+#lab_enc = preprocessing.LabelEncoder()
+#y_train = lab_enc.fit_transform(y_train)
+
+
+# In[17]:
+
+
+knn5 = KNeighborsRegressor(n_neighbors = 5)
+knn5.fit(x_train, y_train)
+
+
+# In[18]:
+
+
+y_test_pred = knn5.predict(x_test)
+
+
+# In[19]:
+
+
+mean_squared_error(y_test_pred, y_test)
+
+
+# In[20]:
+
+
+y_train_pred = knn5.predict(x_train)
+mean_squared_error(y_train_pred, y_train)
+
+
+# In[21]:
+
+
+knn3 = KNeighborsRegressor(n_neighbors = 3)
+knn3.fit(x_train, y_train)
+
+
+# In[22]:
+
+
+y_test_pred = knn3.predict(x_test)
+
+
+# In[23]:
+
+
+mean_squared_error(y_test_pred,  y_test)
+
+
+# In[24]:
+
+
+y_train_pred = knn3.predict(x_train)
+mean_squared_error(y_train_pred, y_train)
+
+
+# In[25]:
+
+
+knn10 = KNeighborsRegressor(n_neighbors = 10)
+knn10.fit(x_train, y_train)
+
+
+# In[26]:
+
+
+y_test_pred = knn10.predict(x_test)
+
+
+# In[27]:
+
+
+mean_squared_error(y_test_pred, y_test)
+
+
+# In[28]:
+
+
+y_train_pred = knn10.predict(x_train)
+mean_squared_error(y_train_pred, y_train)
+
+
+# In[29]:
+
+
+train_mses=[]
+for k in range(1, 10):
+    print("Now computing the MSE for k = ", k)
+    iknn = KNeighborsRegressor(n_neighbors = k)
+    iknn.fit(x_train, y_train)
+    iknn_y_pred = iknn.predict(x_train)
+    mse = mean_squared_error(iknn_y_pred, y_train)
+    train_mses.append(mse)
+
+
+# In[30]:
+
+
+mses=[]
+for k in range(1, 10):
+    print("Now computing the MSE for k = ", k)
+    iknn = KNeighborsRegressor(n_neighbors = k)
+    iknn.fit(x_train, y_train)
+    iknn_y_pred = iknn.predict(x_test)
+    mse = mean_squared_error(iknn_y_pred, y_test)
+    mses.append(mse)
+
+
+# In[31]:
+
+
+mses
+
+
+# In[32]:
+
+
+plt.plot(mses)
+plt.plot(train_mses)
+plt.legend(["Test MSE", "Train MSE"])
+plt.title("The MSE for the number of k")
+plt.xlabel("Numbers of k")
+plt.ylabel("MSE of train and test data")
+
+
+# In[33]:
+
+
+###The model is overfitting when k is at 0 to 3, after that the model becomes underfitting
+
+
+# In[37]:
+
+
+mses_tree =[]
+train_mses_tree = []
+for k in range(1,10):
+    ireg = DecisionTreeRegressor(max_depth = k)
+    ireg.fit(x_train, y_train)
+    ireg_y_pred = ireg.predict(x_test)
+    ireg_y_train = ireg.predict(x_train)
+    mse_train = mean_squared_error(ireg_y_train, y_train)
+    mse_test = mean_squared_error(ireg_y_pred, y_test)
+    mses_tree.append(mse_test)
+    train_mses_tree.append(mse_train)
+mses_tree
+
+
+# In[38]:
+
+
+plt.plot(train_mses_tree)
+plt.plot(mses_tree)
+plt.legend(["Test MSE", "Train MSE"])
+plt.title("The MSE for the number of k")
+plt.xlabel("Numbers of k")
+plt.ylabel("MSE of train and test data")
+
+
+# In[ ]:
+
+
+###The model is overfitting at k is 0 and 1, after that model becomes underfitting
 
